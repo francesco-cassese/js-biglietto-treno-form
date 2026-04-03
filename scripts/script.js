feeddc3cbf76670c75f72863d4147af00f2f8c3c
Francesco Cassese <francescocassese9@gmail.com>
2026-04-03 19:34:24 +0200

    Aggiunto event.preventDefault

--- scripts/script.js
+++ scripts/script.js
@@ -4,35 +4,54 @@
 
 const inputNome = document.querySelector('.input-name');
 const inputKm = document.querySelector('.input-km');
-const selectEta = document.querySelector('.select-eta');
+const selectEta = document.querySelector('.select-age');
+const btnTicket = document.querySelector('#btn-ticket');
+const formTicket = document.querySelector('#ticket-form');
 
-// --- Variabili ---
 
-let datiValidati = false;
+const rimuoviRefresh = event => {
+    event.preventDefault();
 
-// --- CONTROLLI ---
-const nomeValidato = controllaStringa(inputNome.value, pulisciTesto);
+    // --- Variabili ---
 
-//Controllo form nome
-if (nomeValidato === -1) {                                      //Se la funzione restituisce -1 (Campo vuoto)
-    alert('Bello il tuo nome..., ah no non lo hai scritto');    //Alert
-} else {
+    let datiValidati = false;
+    let numeroValidato = 0;
 
-    //Controllo form Km
-    const numeroValidato = controllaNumero(inputKm.value);
+    // --- CONTROLLI ---
 
-    if (numeroValidato === -1) {                                //Se la funzione restituisce -1 (Campo vuoto)
-        alert('I km non si scrivono da soli... Purtroppo!!!');  //Alert
-    }
-    else if (numeroValidato === 0) {                             //Se restituisce 0 (L'utente non ha scritto numeri)
-        alert('I chilometri non si scrivono... si contano! Usa le cifre per favore.'); //alert
+    const nomeValidato = controllaStringa(inputNome.value, pulisciTesto);
 
-    } else if (numeroValidato === 1) {                           //Se restituisce 1 (Ha aggiunto un numero negativo o 0)
-        alert('Anche il viaggio più corto inizia con un passo... almeno una DAI!!!'); //alert
+    //Controllo form nome
+    if (nomeValidato === -1) {                                      //Se la funzione restituisce -1 (Campo vuoto)
+        alert('Bello il tuo nome..., ah no non lo hai scritto');    //Alert
     } else {
-        datiValidati = true;
+
+        //Controllo form Km
+        numeroValidato = controllaNumero(inputKm.value);
+
+        if (numeroValidato === -1) {                                //Se la funzione restituisce -1 (Campo vuoto)
+            alert('I km non si scrivono da soli... Purtroppo!!!');  //Alert
+        }
+        else if (numeroValidato === 0) {                             //Se restituisce 0 (L'utente non ha scritto numeri)
+            alert('I chilometri non si scrivono... si contano! Usa le cifre per favore.'); //alert
+
+        } else if (numeroValidato === 1) {                           //Se restituisce 1 (Ha aggiunto un numero negativo o 0)
+            alert('Anche il viaggio più corto inizia con un passo... almeno una DAI!!!'); //alert
+        } else {
+            datiValidati = true;
+        }
+    }
+
+
+    if (datiValidati) {
+
+        const etaSelezionata = parseInt(selectEta.value);
+
+        const prezzoTotaleBiglietto = calcoloBiglietto(etaSelezionata, numeroValidato);
+
+        console.log(`il passeggero ${nomeValidato} di età ${etaSelezionata} percorrerà ${numeroValidato}km quindi pagherà: ${prezzoTotaleBiglietto}`);
     }
 }
 
-if (datiValidati) { }
+formTicket.addEventListener('submit', rimuoviRefresh);
 
