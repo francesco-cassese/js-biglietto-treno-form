2e9b8309e1d4cba6e8698e681a064bdaead76ff1
Francesco Cassese <francescocassese9@gmail.com>
2026-04-03 17:41:57 +0200

    aggiunta logica validazione al main

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,35 @@
 
 const inputNome = document.querySelector('.input-name');
 const inputKm = document.querySelector('.input-km');
-const selectEta = document.querySelector('select-eta');
+const selectEta = document.querySelector('.select-eta');
+
+// --- Variabili ---
+
+let datiValidati = false;
+
+// --- CONTROLLI ---
+const nomeValidato = controllaStringa(inputNome.value, pulisciTesto);
+
+//Controllo form nome
+if (nomeValidato === -1) {                                      //Se la funzione restituisce -1 (Campo vuoto)
+    alert('Bello il tuo nome..., ah no non lo hai scritto');    //Alert
+} else {
+
+    //Controllo form Km
+    const numeroValidato = controllaNumero(inputKm.value);
+
+    if (numeroValidato === -1) {                                //Se la funzione restituisce -1 (Campo vuoto)
+        alert('I km non si scrivono da soli... Purtroppo!!!');  //Alert
+    }
+    else if (numeroValidato === 0) {                             //Se restituisce 0 (L'utente non ha scritto numeri)
+        alert('I chilometri non si scrivono... si contano! Usa le cifre per favore.'); //alert
+
+    } else if (numeroValidato === 1) {                           //Se restituisce 1 (Ha aggiunto un numero negativo o 0)
+        alert('Anche il viaggio più corto inizia con un passo... almeno una DAI!!!'); //alert
+    } else {
+        datiValidati = true;
+    }
+}
+
+if (datiValidati) { }
+
